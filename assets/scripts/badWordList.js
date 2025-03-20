689ffe334d2baa92f115ac6f0e6d0e0b47174230
jaguarkid817 <methebestestofall@gmail.com>
2025-03-20 09:52:07 -0400

    cool stuff

---
+++ assets/scripts/badWordList.js
@@ -0,0 +1,22 @@
+localList = [
+
+    'ass',
+    'bastard',
+    'bitch',
+    'cock',
+    'cunt',
+    'dildo',
+    'douche',
+    'douchebag',
+    'fag',
+    'fuck',
+    'hell',
+    'masterbate',
+    'nigger;',
+    'orgasm',
+    'penis',
+    'pussy',
+    'shit',
+    'vagina',
+    'whore'
+];
